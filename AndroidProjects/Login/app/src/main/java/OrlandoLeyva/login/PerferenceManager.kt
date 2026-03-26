96ad1587723e0b87636271b21ff158833473ac90
OrlandoLeyvaFontes <orlando.leyva245512@potros.itson.edu.mx>
2026-03-26 16:47:50 -0700

    Terminada act en clase
    Act en clase que por culpa de la compu tuve que terminarla en casa

--- AndroidProjects/Login/app/src/main/java/OrlandoLeyva/login/PerferenceManager.kt
+++ AndroidProjects/Login/app/src/main/java/OrlandoLeyva/login/PerferenceManager.kt
@@ -1,4 +1,21 @@
 package OrlandoLeyva.login
 
-class PerferenceManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+class PreferenceManager(context: Context) {
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences( "user_prefs",  Context.MODE_PRIVATE)
+
+    fun saveLoginStatus(isLoggedIn: Boolean) {
+        sharedPreferences.edit().putBoolean("is_logged_in",  isLoggedIn).apply()
+    }
+
+    fun isLoggedIn(): Boolean {
+        return sharedPreferences.getBoolean(  "is_logged_in",  false)
+    }
+
+    fun logout() {
+        sharedPreferences.edit().clear().apply()
+    }
 }
